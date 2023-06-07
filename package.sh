afbd820e97981ccf623eb436896a6aed420c23e0
Steve Fraser <steve.fraser@weave.works>
2023-06-07 15:02:32 +0000

    remove annoymous mode

--- package.sh
+++ package.sh
@@ -1,4 +1,5 @@
 flux push artifact --path ./ \
     --source="$(git config --get remote.origin.url)" \
-    -revision=main \
+    --revision=main \
+    --creds steve-frser:$GITHUB_TOKEN \
     oci://ghcr.io/steve-fraser/flamingo/manifests:v2.7
